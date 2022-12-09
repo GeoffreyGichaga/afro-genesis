b99de6a5670dfa5178f937a6945658eb84d2c85c
Jeff <geofreygichaga@gmail.com>
2022-12-09 05:10:30 +0300

    fx merge error

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,55 @@
 import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import '../Styling/home.css'
+
+
+
+
+
+
+
+
+
 
 const Home = () => {
   return (
+    <>
+    <Container fluid id='landingcont'>
+     
+     {/* Navigation Bar  */}
+      <Nav className="justify-content-end">
+          <Nav.Item>
+            <Nav.Link href='#' className='navlinks' >Home</Nav.Link>
+          </Nav.Item>
 
-    <div>Home
-      This is the homepage
-    </div>
+          <Nav.Item>
+            <Nav.Link href='#' className='navlinks'>About</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href='#' className='navlinks'>Team</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href='#' className='navlinks'>Products</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href='#' className='navlinks'>Contact</Nav.Link>
+          </Nav.Item>
+
+         
+          
+      </Nav>
+
+      {/* Navigation Bar  */}
+
+
+
+    </Container>
+    
+    </>
   )
 }
 
